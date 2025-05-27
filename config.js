44f3f232bbb9aa8695734853c05097fe651f3425
Abraham77967 <abrahamg7797@gmail.com>
2025-05-27 17:11:12 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const SPOTIFY_CONFIG = {
     
     // The redirect URI registered with your Spotify application
     // This MUST match exactly what you registered in the Spotify Developer Dashboard
-    REDIRECT_URI: "https://abraham77967.github.io/Planify-test-main",
+    REDIRECT_URI: "https://abraham77967.github.io/Planify-test/",
     
     // Scopes determine what permissions your app requests from users
     SCOPES: [
